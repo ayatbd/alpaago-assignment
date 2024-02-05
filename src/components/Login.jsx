178bf3ad49c28394d7a67add35655d722667c2b5
ayatbd <albehestione@gmail.com>
2024-02-05 18:23:57 +0600

    updated some components

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,99 +1,77 @@
-/* eslint-disable react/prop-types */
-import { createContext, useEffect, useState } from "react";
-import {
-  GoogleAuthProvider,
-  createUserWithEmailAndPassword,
-  getAuth,
-  onAuthStateChanged,
-  signInWithEmailAndPassword,
-  signInWithPopup,
-  signOut,
-  updateProfile,
-} from "firebase/auth";
-import axios from "axios";
-import Loader from "../pages/Shared/Loader";
-import { app } from "./../firebase/firebase.config";
-
-export const AuthContext = createContext(null);
-
-const auth = getAuth(app);
-
-const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  const googleProvider = new GoogleAuthProvider();
-
-  const createUser = (email, password) => {
-    setLoading(true);
-    return createUserWithEmailAndPassword(auth, email, password);
-  };
-
-  const signIn = (email, password) => {
-    setLoading(true);
-    return signInWithEmailAndPassword(auth, email, password);
-  };
-
-  const googleSignIn = () => {
-    setLoading(true);
-    return signInWithPopup(auth, googleProvider);
-  };
-
-  const logOut = () => {
-    setLoading(true);
-    return signOut(auth);
-  };
-
-  const updateUserProfile = (name, photo) => {
-    return updateProfile(auth.currentUser, {
-      displayName: name,
-      photoURL: photo,
-    });
-  };
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-      console.log("current user", currentUser);
-
-      if (currentUser) {
-        axios
-          .post(`${import.meta.env.VITE_API_URL}/jwt`, {
-            email: currentUser.email,
-          })
-          .then((data) => {
-            // console.log(data.data.token)
-            localStorage.setItem("access-token", data.data.token);
-          });
-      } else {
-        localStorage.removeItem("access-token");
-      }
-      setLoading(false);
-    });
-
-    return () => {
-      return unsubscribe();
-    };
-  }, []);
-
-  if (loading) {
-    return <Loader></Loader>;
-  }
-
-  const authInfo = {
-    user,
-    loading,
-    setLoading,
-    createUser,
-    signIn,
-    googleSignIn,
-    logOut,
-    updateUserProfile,
-  };
-
+/* eslint-disable react/no-unescaped-entities */
+const Login = () => {
   return (
-    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+    <div>
+      <div className="bg-white lg:w-4/12 md:6/12 w-10/12 m-auto my-10 shadow-md">
+        <div className="py-8 px-8 rounded-xl">
+          <h1 className="font-medium text-2xl mt-3 text-center">Login</h1>
+          <htmlForm action="" className="mt-6">
+            <div className="my-5 text-sm">
+              <label htmlFor="username" className="block text-black">
+                Username
+              </label>
+              <input
+                type="text"
+                autoFocus
+                id="username"
+                className="rounded-sm px-4 py-3 mt-3 focus:outline-none bg-gray-100 w-full"
+                placeholder="Username"
+              />
+            </div>
+            <div className="my-5 text-sm">
+              <label htmlFor="password" className="block text-black">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="rounded-sm px-4 py-3 mt-3 focus:outline-none bg-gray-100 w-full"
+                placeholder="Password"
+              />
+              <div className="flex justify-end mt-2 text-xs text-gray-600">
+                <a href="#">htmlForget Password?</a>
+              </div>
+            </div>
+
+            <button className="block text-center text-white bg-gray-800 p-3 duration-300 rounded-sm hover:bg-black w-full">
+              Login
+            </button>
+          </htmlForm>
+
+          <div className="flex md:justify-between justify-center items-center mt-10">
+            <div className="bg-gray-300 md:block hidden w-4/12"></div>
+            <p className="md:mx-2 text-sm font-light text-gray-400">
+              {" "}
+              Login With Social{" "}
+            </p>
+            <div className="bg-gray-300 md:block hidden w-4/12"></div>
+          </div>
+
+          <div className="grid md:grid-cols-2 gap-2 mt-7">
+            <div>
+              <button className="text-center w-full text-white bg-blue-900 p-3 duration-300 rounded-sm hover:bg-blue-700">
+                Facebook
+              </button>
+            </div>
+            <div>
+              <button className="text-center w-full text-white bg-blue-400 p-3 duration-300 rounded-sm hover:bg-blue-500">
+                Twitter
+              </button>
+            </div>
+          </div>
+
+          <p className="mt-12 text-xs text-center font-light text-gray-400">
+            {" "}
+            Don't have an account?{" "}
+            <a href="#" className="text-black font-medium">
+              {" "}
+              Create One{" "}
+            </a>{" "}
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
-export default AuthProvider;
+export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./components/HomePage";
+import Login from "./components/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "login",
+    element: <Login />,
   },
 ]);
 
